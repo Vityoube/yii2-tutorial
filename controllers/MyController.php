a89f5b81dc72f735c1bb30f0489f807b7795c891
vityoube <vityoube@gmail.com>
2017-07-18 20:12:23 +0200

    Added AppController

--- controllers/MyController.php
+++ controllers/MyController.php
@@ -10,19 +10,24 @@ namespace app\controllers;
 
 use yii\web\Controller;
 
+
 /**
  * Description of MyController
  *
  * @author vkalashnykov
  */
-class MyController extends Controller{
+class MyController extends AppController{
     //put your code here
-    
+
     public function actionIndex($id=null){
         $hi ='Hello World!';
         $names=['Ivanov','Petrov','Sidorov'];
         if (!$id)$id='test';
 //        return $this->render('index',['hello'=>$hi,'names'=>$names]);
         return $this->render('index',compact('hi','names','id'));
+    }
+    
+    public function actionBlogPost(){
+        return 'Blog Post!';
     }
 }
